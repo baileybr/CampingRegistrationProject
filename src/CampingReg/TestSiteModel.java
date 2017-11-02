78d13a7a789cc9f3060767007af530200cff8edd
baileybr <baileybr@mail.gvsu.edu>
2017-11-02 16:06:47 -0400

    Created TestSiteModel class and started, but have currently hit a few roadblocks.

---
+++ src/CampingReg/TestSiteModel.java
@@ -0,0 +1,150 @@
+package CampingReg;
+
+import static org.junit.Assert.*;
+
+import java.util.GregorianCalendar;
+
+import org.junit.Test;
+
+
+/**********************************************************************
+ * JUnit Testing for SiteModel.java
+ * 
+ * @author Brendan Bailey
+ *
+ *********************************************************************/
+public class TestSiteModel {
+
+	@Test
+	public void testRowCount() {
+		SiteModel s = new SiteModel();
+		RV r = new RV();
+		s.add(r);
+		assertTrue(s.getRowCount() == 1);
+	}
+	
+	@Test
+	public void testRowCount2() {
+		SiteModel s = new SiteModel();
+		Tent t = new Tent();
+		s.add(t);
+		assertTrue(s.getRowCount() == 1);
+	}
+	
+	@Test
+	public void testRowCount3() {
+		SiteModel s = new SiteModel();
+		RV r = new RV();
+		Tent t = new Tent();
+		s.add(r);
+		s.add(t);
+		assertTrue(s.getRowCount() == 2);
+	}
+	
+	@Test
+	public void testRowCount4() {
+		SiteModel s = new SiteModel();
+		RV r = new RV();
+		RV r2 = new RV();
+		s.add(r);
+		s.add(r2);
+		assertTrue(s.getRowCount() == 2);
+	}
+	
+	@Test
+	public void testRowCount5() {
+		SiteModel s = new SiteModel();
+		Tent t = new Tent();
+		Tent t2 = new Tent();
+		s.add(t);
+		s.add(t2);
+		assertTrue(s.getRowCount() == 2);
+	}
+	
+	@Test
+	public void testRowCount6() {
+		SiteModel s = new SiteModel();
+		assertTrue(s.getRowCount() == 0);
+	}
+	
+	@Test
+	public void testColumnCount() {
+		SiteModel s = new SiteModel();
+		RV r = new RV();
+		s.add(r);
+		assertTrue(s.getColumnCount() == 5);
+	}
+	
+	@Test
+	public void testColumnCount2() {
+		SiteModel s = new SiteModel();
+		Tent t = new Tent();
+		s.add(t);
+		assertTrue(s.getColumnCount() == 5);
+	}
+	
+	@Test
+	public void testColumnCount3() {
+		SiteModel s = new SiteModel();
+		Tent t = new Tent();
+		RV r = new RV();
+		s.add(t);
+		s.add(r);
+		assertTrue(s.getColumnCount() == 5);
+	}
+	
+	@Test
+	public void testColumnName() {
+		SiteModel s = new SiteModel();
+		assertTrue(s.getColumnName(0) == "Name Reserving");
+	}
+	
+	@Test
+	public void testColumnName2() {
+		SiteModel s = new SiteModel();
+		assertTrue(s.getColumnName(1) == "Checked in");
+	}
+	
+	@Test
+	public void testColumnName3() {
+		SiteModel s = new SiteModel();
+		assertTrue(s.getColumnName(2) == "Days Staying");
+	}
+	
+	@Test
+	public void testColumnName4() {
+		SiteModel s = new SiteModel();
+		assertTrue(s.getColumnName(3) == "Site #");
+	}
+	
+	@Test
+	public void testColumnName5() {
+		SiteModel s = new SiteModel();
+		assertTrue(s.getColumnName(4) == "Tent/RV info");
+	}
+	
+	@Test
+	public void testValueAt() throws Exception {
+		SiteModel s = new SiteModel();
+		Tent t = new Tent();
+		GregorianCalendar g = new GregorianCalendar(2017, 10, 31);
+		t.setNameReserving("John Doe");
+		t.setCheckIn(g);
+		t.setDaysStaying(1);
+		t.setSiteNumber(1);
+		t.setNumOfTenters(1);
+		s.add(t);
+		assertTrue(s.getValueAt(0, 0) == "John Doe");
+		assertTrue(s.getValueAt(0, 1) == "10/31/2017");
+		assertTrue(s.getValueAt(0, 2) == "1");
+		assertTrue(s.getValueAt(0, 3) == "1");
+		assertTrue(s.getValueAt(0, 4) == "1 tenter");
+	}
+	
+	@Test
+	public void testCurrentSites() {
+		SiteModel s = new SiteModel();
+		Tent t = new Tent();
+		//assertTrue(s.getCurrentSites() == t);
+	}
+}
